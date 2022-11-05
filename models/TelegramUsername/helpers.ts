023c017be710c54544c91a3531707b4bd618b8f4
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-11-05 13:51:18 +0700

    Clean libs/telegram

---
+++ models/TelegramUsername/helpers.ts
@@ -0,0 +1,34 @@
+import { Cage } from 'zenbox-util/cage'
+import { ensureFind } from 'zenbox-util/ensure'
+import { Contact, parseContact } from '../../../generic/models/Contact'
+import { Person } from '../../../generic/models/Person'
+import { TelegramUsernameSchema } from '../TelegramUsername'
+
+export function ensureTelegramContact<Err>(person: Person, error?: Cage<Err>): Contact {
+  return ensureFind(person.contacts, c => c.startsWith(telegramUrlPrefix), error)
+}
+
+export function ensureTelegramUsername<Err>(person: Person, error?: Cage<Err>): string {
+  return getTelegramUsernameFromUrl(ensureTelegramContact(person, error))
+}
+
+export function getTelegramUsernameFromUrl(url: string) {
+  const contactURL = new URL(url)
+  const protocol = contactURL.protocol.replace(':', '')
+  switch (protocol) {
+    case 'telegram':
+    case 'tg':
+      return contactURL.pathname
+    case 'http':
+    case 'https':
+      return contactURL.pathname.split('/')[1]
+    default:
+      throw new Error(`Unknown contact protocol: ${protocol} (url: ${url})`)
+  }
+}
+
+export const telegramUrlPrefix = 'https://t.me'
+
+export function tg(username: string) {
+  return parseContact(telegramUrlPrefix + '/' + TelegramUsernameSchema.parse(username))
+}
